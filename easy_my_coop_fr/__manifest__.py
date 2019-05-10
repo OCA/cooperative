52adbb94480d04889888792e8976f1ca75d6e27c
houssine <houssine.bakkali@gmail.com>
2019-05-10 16:42:13 +0200

    [MIG] migrate module to v12

---
+++ easy_my_coop_fr/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2013-2018 Open Architects Consulting SPRL.
+# Copyright 2018-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).#
+{
+    "name": "Easy My Coop Fr",
+    "version": "12.0.1.0.0",
+    "depends": ["easy_my_coop",
+                "l10n_fr"],
+    "author": "Houssine BAKKALI <houssine@coopiteasy.be>",
+    "category": "Cooperative management",
+    'webstite': "www.coopiteasy.be",
+    "license": "AGPL-3",
+    "description": """
+    This is the french localization for the easy my coop module
+    """,
+    'data': [
+        'views/subscription_template.xml',
+        # 'views/layout_template.xml',
+        'views/certificate_template.xml'
+    ],
+    'installable': True,
+}
