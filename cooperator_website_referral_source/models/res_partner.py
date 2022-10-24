coopiteasy/vertical-cooperative

[ADD] cooperator_website_referral_source

cooperator_website_referral_source/models/res_partner.py
@@ -1,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    referral_source_id = fields.Many2one(
+        comodel_name="referral.source",
+        string="How did you know about us?",
+    )

OCA/cooperative

cooperator_website_referral_source/models/res_partner.py
@@ -1,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    referral_source_id = fields.Many2one(
+        comodel_name="referral.source",
+        string="How did you hear about us?",
+    )