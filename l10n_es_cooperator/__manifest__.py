287709e67d854679fd68af3f7f57cb757bd40abf
César López Ramírez <cesar.lopez@coopdevs.org>
2023-01-31 16:40:25 +0100

    [ADD] l10n_es_cooperator: Add VAT for partner in cooperator module

---
+++ l10n_es_cooperator/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Cooperator Spain Localisation",
+    "version": "12.0.0.1.0",
+    "depends": ["cooperator", "cooperator_website", "l10n_es"],
+    "author": (
+        "Coop IT Easy SC,"
+        "Coopdevs Treball SCCL,"
+        "Odoo Community Association (OCA)"
+    ),
+    "mantainer": "Coopdevs Treball SCCL",
+    "website": "https://github.com/OCA/cooperative",
+    "category": "Cooperative management",
+    "summary": """
+    Cooperator localization for Spain
+    """,
+    "license": "AGPL-3",
+    "data": [
+        "views/subscription_request_view.xml",
+        "views/subscription_templates.xml",
+    ],
+    "installable": True,
+    "auto-install": True,
+}
