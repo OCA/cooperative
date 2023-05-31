0393caefc1c6ef4d69d24dfda50d32262f00933a
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-05-31 13:20:15 +0200

    [REF] l10n_be_cooperator_national_number: Separate display from require
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ l10n_be_cooperator_national_number/readme/USAGE.rst
@@ -0,0 +1,4 @@
+On the company, two new toggles 'Display National Number' and 'Require National
+Number' are added. In order to expose the functionality of this module, you must
+enable these toggles for every company that wishes to use it. 'Display' shows
+the field and allows it to be filled in. 'Require' makes the field mandatory.
--- l10n_be_cooperator_national_number/tests/test_cooperator_national_number.py
+++ l10n_be_cooperator_national_number/tests/test_cooperator_national_number.py
@@ -2,7 +2,7 @@
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tests.common import SavepointCase
 
 from odoo.addons.cooperator.tests.cooperator_test_mixin import CooperatorTestMixin
@@ -20,7 +20,32 @@ class TestCooperatorNationalNumber(SavepointCase, CooperatorTestMixin):
 
     def set_national_number_required(self):
         company = self.env.company
+        company.display_national_number = True
         company.require_national_number = True
+
+    def test_require_without_display(self):
+        """You cannot require a national number without also displaying it."""
+        company = self.env.company
+        company.write(
+            {
+                "display_national_number": False,
+                "require_national_number": False,
+            }
+        )
+        with self.assertRaises(ValidationError):
+            company.require_national_number = True
+        self.set_national_number_required()
+        with self.assertRaises(ValidationError):
+            company.display_national_number = False
+
+    def test_company_not_required(self):
+        """Subscription requests for companies do not require a national number."""
+        self.set_national_number_required()
+        vals = self.get_dummy_company_subscription_requests_vals()
+        subscription_request = self.env["subscription.request"].create(vals)
+        subscription_request.validate_subscription_request()
+        self.assertFalse(subscription_request.display_national_number)
+        self.assertFalse(subscription_request.require_national_number)
 
     def test_national_number_applied_to_partner(self):
         self.set_national_number_required()
