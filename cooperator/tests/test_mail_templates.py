4c8318302b719103f26fd727a7de06fa9014cf30
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-06-06 17:00:07 +0200

    cooperator: Scaffolding for multi-company e-mail templates
    In this commit, the confirmation e-mail template (chosen at random) is
    configured on the res.company model instead of fetched via its XMLID. In
    a future commit, all the other cooperator-related e-mail templates will
    be configured in the same way, using the common scaffolding from this
    commit.
    
    The commit introduces a bug/regression, which is that the confirmation
    e-mail won't actually be sent. This is a result of the new scaffolding.
    Before, this happened:
    
    1. cooperator is marked installed.
    2. The mail template data file gets sourced, creating the confirmation
       template's record.
    3. The subscription request demo data file gets sourced, creating a
       subscription.request record.
    4. While creating the record, an e-mail using the confirmation template
       gets sent.
    
    In the new logic, this happens:
    
    1. cooperator is marked installed.
    2. The mail template data file gets sourced, creating the confirmation
       template's record, but NOT setting it on company 1.
    3. The subscription request demo data file gets sourced, creating a
       subscription.request record.
    4. While creating the record, Odoo tries to send a mail of the template
       company_id.cooperator_confirmation_mail_template, but the field is
       empty.
    5. Error, obviously, but we've commented out the erroneous code, so
       let's proceed.
    6. As a post-init hook, a copy of the confirmation template gets set on
       all companies' cooperator_confirmation_mail_template field.
    
    We'll need to re-jig the logic to circumvent the error. A problem for
    future me. Or you, if you're reading this in the distant future. Hello.
    
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ cooperator/tests/test_mail_templates.py
@@ -0,0 +1,95 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from odoo.tests.common import SavepointCase
+
+
+class TestMailTemplates(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.default_template = cls.env.ref("cooperator.email_template_confirmation")
+
+    def test_new_company_gets_copy_of_template(self):
+        """When creating a company, they should correctly get a copy of the mail
+        template.
+        """
+        company = self.env["res.company"].create({"name": "Test Company"})
+        new_template = company.cooperator_confirmation_mail_template
+        self.assertIn(company.name, new_template.name)
+
+    def test_existing_company_gets_copy_of_template(self):
+        """When running the default method on an existing company, they should
+        correctly get a copy of the mail template. This mirrors the posthook.
+        """
+        company = self.env["res.company"].create({"name": "Test Company"})
+        company.cooperator_confirmation_mail_template = False
+
+        company._assign_default_cooperator_mail_template(
+            "cooperator_confirmation_mail_template",
+            "cooperator.email_template_confirmation",
+        )
+        new_template = company.cooperator_confirmation_mail_template
+        self.assertIn(company.name, new_template.name)
+
+    def test_assign_default_mail_template_no_copy(self):
+        """When providing copy=False, the global template is used."""
+        company = self.env["res.company"].create({"name": "Test Company"})
+        company.cooperator_confirmation_mail_template = False
+
+        company._assign_default_cooperator_mail_template(
+            "cooperator_confirmation_mail_template",
+            "cooperator.email_template_confirmation",
+            copy=False,
+        )
+        new_template = company.cooperator_confirmation_mail_template
+        self.assertEqual(new_template, self.default_template)
+
+    def test_assign_no_override(self):
+        """When instructing not to override, don't."""
+        company = self.env["res.company"].create({"name": "Test Company"})
+        template = company.cooperator_confirmation_mail_template
+
+        company._assign_default_cooperator_mail_template(
+            "cooperator_confirmation_mail_template",
+            "cooperator.email_template_confirmation",
+            override=False,
+        )
+        new_template = company.cooperator_confirmation_mail_template
+        self.assertEqual(template, new_template)
+
+    def test_assign_override(self):
+        """When instructing to override, do."""
+        company = self.env["res.company"].create({"name": "Test Company"})
+        template = company.cooperator_confirmation_mail_template
+
+        company._assign_default_cooperator_mail_template(
+            "cooperator_confirmation_mail_template",
+            "cooperator.email_template_confirmation",
+            override=True,
+        )
+        new_template = company.cooperator_confirmation_mail_template
+        self.assertNotEqual(template, new_template)
+
+    def test_create_company_dont_override_vals(self):
+        """When defining a field in the create method, don't override it."""
+        company = self.env["res.company"].create(
+            {
+                "name": "Test Company",
+                "cooperator_confirmation_mail_template": self.default_template.id,
+            }
+        )
+        self.assertEqual(
+            company.cooperator_confirmation_mail_template, self.default_template
+        )
+
+    def test_create_company_override_falsy_val(self):
+        """When 'defining' a field in the create method as falsy, do override it."""
+        company = self.env["res.company"].create(
+            {
+                "name": "Test Company",
+                "cooperator_confirmation_mail_template": False,
+            }
+        )
+        self.assertTrue(company.cooperator_confirmation_mail_template)
