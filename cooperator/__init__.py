403d2f6fde735f0eacba60620ad87cb19c9f0d84
hugues de keyzer <odoo@hugues.info>
2023-06-07 11:28:45 +0200

    [IMP] create subscription journal per company

--- cooperator/__init__.py
+++ cooperator/__init__.py
@@ -1,3 +1,13 @@
+from odoo import api, SUPERUSER_ID
+
 from . import models
 from . import report
 from . import wizard
+
+
+def post_init(cr, registry):
+    # the subscription journal must be created for each company.
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    subscription_request_model = env["subscription.request"]
+    for company in env.companies:
+        subscription_request_model.create_journal(company)
