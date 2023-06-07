c455d57bb1ed4c87b808511b394ea5b14f55e027
hugues de keyzer <odoo@hugues.info>
2023-06-07 17:00:34 +0200

    ensure demo data is correctly created

--- cooperator/tests/cooperator_test_mixin.py
+++ cooperator/tests/cooperator_test_mixin.py
@@ -9,43 +9,8 @@ class CooperatorTestMixin:
     @classmethod
     def set_up_cooperator_test_data(cls):
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        # accounting data needs to be created even if created in module data
-        # because when launching tests, accounting data will be deleted when odoo loads
-        # a test chart of account.
-        # cf _load in chart_template.py in account module
-
-        account_model = cls.env["account.account"]
-        cls.company = cls.env.user.company_id
-        cls.company.coop_email_contact = "coop_email@example.org"
+        cls.env.company.coop_email_contact = "coop_email@example.org"
         cls.demo_partner = cls.env.ref("base.partner_demo")
-
-        receivable_account_type = cls.env.ref("account.data_account_type_receivable")
-        equity_account_type = cls.env.ref("account.data_account_type_equity")
-        cooperator_account = account_model.create(
-            {
-                "name": "Cooperator Test",
-                "code": "416109",
-                "user_type_id": receivable_account_type.id,
-                "reconcile": True,
-            }
-        )
-        cls.company.property_cooperator_account = cooperator_account
-        cls.equity_account = account_model.create(
-            {
-                "name": "Equity Test ",
-                "code": "100919",
-                "user_type_id": equity_account_type.id,
-                "reconcile": True,
-            }
-        )
-        cls.subscription_journal = cls.env["account.journal"].create(
-            {
-                "name": "Subscriptions Test",
-                "code": "SUBJT",
-                "type": "sale",
-            }
-        )
-
         cls.share_x = cls.env["product.product"].create(
             {
                 "name": "Share X - Founder",
@@ -133,7 +98,7 @@ class CooperatorTestMixin:
                         0,
                         {
                             "name": invoice.payment_reference,
-                            "account_id": self.company.property_cooperator_account.id,
+                            "account_id": self.env.company.property_cooperator_account.id,
                             "credit": amount,
                         },
                     ),
