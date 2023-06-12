a7b4a5cdcdfe0dbbedc0d0a32d1595413e9e6e32
hugues de keyzer <odoo@hugues.info>
2023-06-12 14:35:53 +0200

    use xml data instead of post-init hook
    use xml data instead of post-init hook to initialize company data.

--- cooperator/__init__.py
+++ cooperator/__init__.py
@@ -1,10 +1,3 @@
-from odoo import api, SUPERUSER_ID
-
 from . import models
 from . import report
 from . import wizard
-
-
-def post_init(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env.companies._init_cooperator_data()
