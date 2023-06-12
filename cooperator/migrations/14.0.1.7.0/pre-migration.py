2ee57611269d338be9ec46493e36db37b195852f
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-06-12 10:41:49 +0200

    [FIX] cooperator: Set company on sequences in migration script
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- cooperator/migrations/14.0.1.7.0/pre-migration.py
+++ cooperator/migrations/14.0.1.7.0/pre-migration.py
@@ -12,12 +12,18 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     company = env.ref("base.main_company")
+    # Use the existing and already-in-use sequences for company 1.
     if company:
+        # Set sequences on company and company on sequences
+        # First sequence
         company.cooperator_subscription_sequence_id = env.ref(
             "cooperator.sequence_subscription"
         )
+        company.cooperator_subscription_sequence_id.company_id = company
+        # Second sequence
         company.cooperator_register_operation_sequence_id = env.ref(
             "cooperator.sequence_register_operation"
         )
+        company.cooperator_register_operation_sequence_id.company_id = company
     other_companies = env["res.company"].search([]) - company
     other_companies._create_cooperator_sequences()
