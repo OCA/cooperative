091cf962b70732f5da73c87addd882a5c5771770
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-06-13 11:04:35 +0200

    cooperator: Use data function instead of post-init hook to create sequences on company
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- cooperator/__init__.py
+++ cooperator/__init__.py
@@ -1,12 +1,3 @@
 from . import models
 from . import report
 from . import wizard
-
-from odoo import api, SUPERUSER_ID
-
-
-def _create_cooperator_sequences(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-
-    companies = env["res.company"].search([])
-    companies._create_cooperator_sequences()
