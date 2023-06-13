27b9e5c2a45668eaea6f2504ef097057a51a5b6a
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-06-13 11:00:12 +0200

    cooperator: Get sequence by code instead of saving it on company
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- cooperator/migrations/14.0.1.7.0/pre-migration.py
+++ cooperator/migrations/14.0.1.7.0/pre-migration.py
@@ -9,21 +9,36 @@ from odoo import SUPERUSER_ID, api
 _logger = logging.getLogger(__name__)
 
 
+def remove_xmlid(env, module, name):
+    xmlid_record = env["ir.model.data"].search(
+        [("module", "=", module), ("name", "=", name)]
+    )
+    if xmlid_record:
+        xmlid_record.unlink()
+
+
 def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     company = env.ref("base.main_company")
     # Use the existing and already-in-use sequences for company 1.
     if company:
-        # Set sequences on company and company on sequences
-        # First sequence
-        company.cooperator_subscription_sequence_id = env.ref(
-            "cooperator.sequence_subscription"
+        cooperator_number = env.ref("cooperator.sequence_subscription")
+        cooperator_number.write(
+            {
+                "company_id": company.id,
+                "code": "cooperator.number",
+            }
         )
-        company.cooperator_subscription_sequence_id.company_id = company
-        # Second sequence
-        company.cooperator_register_operation_sequence_id = env.ref(
-            "cooperator.sequence_register_operation"
+        register_operation = env.ref("cooperator.sequence_register_operation")
+        register_operation.write(
+            {
+                "company_id": company.id,
+                "code": "register.operation",
+            }
         )
-        company.cooperator_register_operation_sequence_id.company_id = company
+        # Delete the xmlids. The actual records are preserved.
+        remove_xmlid(env, "cooperator", "sequence_subscription")
+        remove_xmlid(env, "cooperator", "sequence_register_operation")
+    # Create sequences for other companies.
     other_companies = env["res.company"].search([]) - company
     other_companies._create_cooperator_sequences()
