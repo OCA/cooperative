c61e8dff3f3ebc9e03592cd4e3d06f501d472759
hugues de keyzer <odoo@hugues.info>
2023-06-16 10:45:40 +0200

    define default share company only in xml

--- cooperator/models/product.py
+++ cooperator/models/product.py
@@ -9,16 +9,6 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    def _default_company_id(self):
-        # when creating a share, the company_id field in the form should be
-        # set by default to the current company, while allowing to set it to
-        # any other value (including none). this seems to work, but is this
-        # the right way to do it? is there a way to set default_company_id to
-        # the current company in the context of the act_window?
-        if self.env.context.get("default_is_share"):
-            return self.env.company
-        return False
-
     is_share = fields.Boolean(string="Is share?")
     short_name = fields.Char(string="Short name")
     display_on_website = fields.Boolean(string="Display on website")
@@ -28,7 +18,6 @@ class ProductTemplate(models.Model):
     by_company = fields.Boolean(string="Can be subscribed by companies?")
     by_individual = fields.Boolean(string="Can be subscribed by individuals?")
     mail_template = fields.Many2one("mail.template", string="Mail template")
-    company_id = fields.Many2one("res.company", default=_default_company_id)
 
     def get_web_share_products(self, is_company):
         if is_company is True:
