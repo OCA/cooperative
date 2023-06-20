9e42d63f217eeb384b4b3ef987dd3a6e5c1899a7
Robin Keunen <robin@keunen.net>
2023-06-20 10:06:07 +0200

    cooperator 14.0.1.6.1

--- cooperator/__manifest__.py
+++ cooperator/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Cooperators",
     "summary": "Manage your cooperators",
-    "version": "14.0.1.6.0",
+    "version": "14.0.1.6.1",
     "depends": [
         "base",
         "web",
