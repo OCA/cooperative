281dfdf5c37408a280728d36bf294108b32bb2bd
hugues de keyzer <odoo@hugues.info>
2023-08-31 16:38:00 +0200

    [IMP] adjust copyright statements
    fix copyright statements: add missing ones and use the same format
    everywhere.

--- cooperator_website/static/src/js/cooperator.js
+++ cooperator_website/static/src/js/cooperator.js
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2017 Open Architects Consulting SPRL
+// SPDX-FileCopyrightText: 2018 Coop IT Easy SC
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 odoo.define("cooperator.oe_cooperator", function (require) {
     "use strict";
     $(document).ready(function () {
