4ba60e9e4db0b54fabb7d7642b4b74d930227518
hugues de keyzer <odoo@hugues.info>
2023-09-01 17:25:56 +0200

    [IMP] move code and rename files
    move classes to separate files and rename some files to improve
    consistency.

--- l10n_be_cooperator/models/__init__.py
+++ l10n_be_cooperator/models/__init__.py
@@ -1,3 +1,5 @@
-from . import partner
+from . import res_partner
 from . import subscription_request
+from . import tax_shelter_certificate
+from . import tax_shelter_certificate_line
 from . import tax_shelter_declaration
--- l10n_be_cooperator/models/subscription_request.py
+++ l10n_be_cooperator/models/subscription_request.py
@@ -1,9 +1,11 @@
 from odoo import fields, models
 
-from . import partner
+from . import res_partner
 
 
 class SubscriptionRequest(models.Model):
     _inherit = "subscription.request"
 
-    company_type = fields.Selection(selection_add=partner.get_company_type_selection())
+    company_type = fields.Selection(
+        selection_add=res_partner.get_company_type_selection()
+    )
