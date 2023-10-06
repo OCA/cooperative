4f1157b58472e839c63975c804cf92778fcda15f
hugues de keyzer <odoo@hugues.info>
2023-10-06 14:05:23 +0200

    [IMP] improve contact views
    * move the "cooperator candidates" menu entry to the "cooperators" menu.
    * use default filters instead of domains in contact views.
    * remove the extra columns in the contacts list view; they can now be
      found in the cooperator register.

--- cooperator/migrations/16.0.0.1.0/post-migration.py
+++ cooperator/migrations/16.0.0.1.0/post-migration.py
@@ -18,6 +18,24 @@ _records_to_delete = [
     "cooperator.email_template_confirmation_company",
 ]
 
+# these act_windows had previously a domain set. simply removing the field
+# element from the xml definition is not enough to reset it to its default
+# value. instead of forcing it to False in the xml, they are reset in this
+# script.
+_act_windows_with_obsolete_domain = [
+    "cooperator.action_partner_cooperator_form",
+    "cooperator.action_partner_cooperator_candidate_form",
+    "cooperator.action_company_representative_form",
+]
+
+
+def remove_act_windows_domain(env, act_window_xml_ids):
+    for xml_id in act_window_xml_ids:
+        act = env.ref(xml_id, raise_if_not_found=False)
+        if not act:
+            continue
+        act.domain = False
+
 
 @openupgrade.migrate()
 def migrate(env, version):
@@ -26,3 +44,4 @@ def migrate(env, version):
         env.cr, "cooperator", _translations_to_delete
     )
     openupgrade.delete_records_safely_by_xml_id(env, _records_to_delete)
+    remove_act_windows_domain(env, _act_windows_with_obsolete_domain)
