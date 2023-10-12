49983c9ee3f8ec63510d8bbe2a935097376ac00b
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-10-12 13:22:55 +0200

    [MIG] l10n_fr_cooperator: Migration to 14.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- l10n_fr_cooperator/models/partner.py
+++ l10n_fr_cooperator/models/partner.py
@@ -10,5 +10,11 @@ class ResPartner(models.Model):
             ("eurl", "EURL / Entreprise individuelle"),
             ("sarl", "SARL"),
             ("sa", "SA / SAS"),
-        ]
+        ],
+        ondelete={
+            "asso": "set null",
+            "eurl": "set null",
+            "sarl": "set null",
+            "sa": "set null",
+        },
     )
