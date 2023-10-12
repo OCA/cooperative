b0f2d0a69887ffd4eaa6c251b21ba179a06cd647
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-10-12 11:56:20 +0200

    [MIG] l10n_fr_cooperator: Migration to 13.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- l10n_fr_cooperator/__manifest__.py
+++ l10n_fr_cooperator/__manifest__.py
@@ -4,11 +4,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).#
 {
     "name": "Cooperator France Localization",
-    "version": "12.0.1.2.0",
+    "version": "13.0.1.0.0",
     "depends": ["cooperator", "cooperator_website", "l10n_fr"],
     "author": "Coop IT Easy SC",
     "category": "Cooperative management",
-    "website": "https://coopiteasy.be",
+    "website": "https://github.com/OCA/cooperative",
     "license": "AGPL-3",
     "summary": """
         This is the French localization for the Cooperators module
