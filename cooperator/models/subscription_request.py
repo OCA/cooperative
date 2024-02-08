cc28eb60942ff5c775dcc5ef3e01e4ed4851e2b5
Victor Champonnois <victor@coopiteasy.be>
2024-02-08 11:29:09 +0100

    [IMP] Set birthdate to required on sub request
    The birthdate is necessary for some later cooperator actions, so it must be set as required.

--- cooperator/models/subscription_request.py
+++ cooperator/models/subscription_request.py
@@ -211,6 +211,7 @@ class SubscriptionRequest(models.Model):
     birthdate = fields.Date(
         string="Date of birth",
         readonly=True,
+        required=True,
         states={"draft": [("readonly", False)]},
     )
     gender = fields.Selection(
