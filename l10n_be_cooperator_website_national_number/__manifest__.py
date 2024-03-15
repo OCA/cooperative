b9bb2cb8b58f255d41225cc90a65d0380740ad5e
Victor Champonnois <victor@coopiteasy.be>
2024-03-15 14:38:05 +0100

    [MIG] l10n_be_cooperator_website_national_number: Migration to 16.0

--- l10n_be_cooperator_website_national_number/__manifest__.py
+++ l10n_be_cooperator_website_national_number/__manifest__.py
@@ -6,7 +6,7 @@
         Ask for Belgian National Number in Cooperative
         Subscription Request Frontend Form.
         """,
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": [
         "l10n_be_cooperator_national_number",
         "l10n_be_cooperator",
