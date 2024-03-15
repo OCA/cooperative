bbf5ca1b0db42202985c52b2e53250e6a28599b7
Victor Champonnois <victor@coopiteasy.be>
2024-03-15 14:39:56 +0100

    [REF] depends on l10n_be_partner_identification

--- l10n_be_cooperator_website_national_number/controllers/main.py
+++ l10n_be_cooperator_website_national_number/controllers/main.py
@@ -11,7 +11,7 @@ class WebsiteSubscription(WebsiteSubscription):
             company = request.env.company
             if not is_company and company.require_national_number:
                 national_number_id_category = request.env.ref(
-                    "l10n_be_national_number.l10n_be_national_number_category"
+                    "l10n_be_partner_identification.l10n_be_national_registry_number_category"
                 ).id
                 national_number = partner.id_numbers.filtered(
                     lambda id_num: id_num.category_id.id == national_number_id_category
