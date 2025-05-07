8be72c27a965af8524205d5ffbb25dcf53834364
Nicolás Ramos <nicolas.ramos@somit.coop>
2025-05-07 08:18:45 +0200

    Clean cooperator directory before applying history

--- cooperator/models/share_type.py
+++
@@ -1,8 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
-#
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
-
-def get_share_types(env):
-    shares = env["product.product"].search([("is_share", "=", True)])
-    return [(s.default_code, s.short_name) for s in shares]
